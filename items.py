0be66cb2b31105f1c57a6d14b747cbdc39b732ea
Oksana Hordych <116382404+sana23ok@users.noreply.github.com>
2025-03-08 17:40:40 +0200

    change id field name

--- items.py
+++ items.py
@@ -1,6 +1,6 @@
 class Item:
-    def __init__(self, ID, weight, value):
-        self.id = ID
+    def __init__(self, item_id, weight, value):
+        self.id = item_id
         self.weight = weight
         self.value = value
 
